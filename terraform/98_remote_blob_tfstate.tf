2a4be7b336435b9fad841a1f85859f9f0c8b7aff
maxencecolmant <maxence.colmant@ynov.com>
2021-01-14 13:36:47 +0100

    add remote tfstate

--- terraform/98_remote_blob_tfstate.tf
+++ terraform/98_remote_blob_tfstate.tf
@@ -4,6 +4,5 @@ terraform {
     storage_account_name = "blobtfstateproject"
     container_name       = "tfstatemaj2"
     key                  =  "terraform.tfstate"
-    #key                  =  "3g4wbpahb5D9vBWqrDKElDDZpAukyzyGOYQKLJD9EGHwDFbAOHFRmbtwd/hWbzkjrEVjSwid/Q08pvFvKYgG5w=="
   }
 }
